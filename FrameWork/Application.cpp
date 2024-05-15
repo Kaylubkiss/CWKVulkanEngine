1f69b7dafc9db6b56d52e3c0b40785cf419c3b5b
Caleb Kissinger <kaylubkiss@gmail.com>
2024-05-15 00:50:31 -0500

    fixed function name for viewport resizing

--- FrameWork/Application.cpp
+++ FrameWork/Application.cpp
@@ -947,7 +947,7 @@ void Application::RecreateSwapChain()
 
 }
 
-void Application::ResizeWindow() 
+void Application::ResizeViewport()
 {
 	int width, height;
 	SDL_GetWindowSizeInPixels(this->window, &width, &height);
