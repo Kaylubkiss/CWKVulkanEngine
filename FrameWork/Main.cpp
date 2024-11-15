ee5d7ef24385cc7f0b169168104b6fbc3744de6a
Caleb Kissinger <kaylubkiss@gmail.com>
2024-11-15 00:58:52 -0800

    cmake list

--- FrameWork/Main.cpp
+++ FrameWork/Main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include "Application.h"
+#include <filesystem>
 
-
+static void Locatecwd()
+{
+    auto cwd = std::filesystem::current_path();
+    while (cwd.has_parent_path() && cwd.parent_path() != cwd)
+    {
+        if (std::filesystem::exists(cwd.string() + "/FrameWork/"))
+        {
+            std::filesystem::current_path(cwd/"FrameWork");
+            return;
+        }
+        cwd = cwd.parent_path();
+    }
+}
 
 int main(int argc, char* argv[])
 {
+
+    Locatecwd();
+
 	_Application->run();
 
 	return 0;
