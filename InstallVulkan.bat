524c585ac0eb0dfc95b98a048a91bc1a262b3681
Caleb Kissinger <kaylubkiss@gmail.com>
2025-01-17 02:11:42 -0800

    changed the batch file to make sure environment variable is set. Also made the project name a variable for easier editing.

--- InstallVulkan.bat
+++ InstallVulkan.bat
@@ -1,22 +1,29 @@
 @echo off
 
 
+
 if defined VULKAN_SDK (
 	echo Vulkan Dependency Found...
 ) else (
+
 	echo VulkanSDK not found, downloading to your repository...
 
 	curl https://sdk.lunarg.com/sdk/download/1.4.304.0/windows/VulkanSDK-1.4.304.0-Installer.exe --output VulkanInstaller.exe
 
 	VulkanInstaller.exe --accept-licenses --default-answer --root "%~dp0\VulkanSDK" --confirm-command install com.lunarg.vulkan.32bit com.lunarg.vulkan.sdl2 com.lunarg.vulkan.glm com.lunarg.vulkan.vma com.lunarg.vulkan.debug com.lunarg.vulkan.debug32
+
+	setx VULKAN_SDK "%~dp0\VulkanSDK"
+
+	set VULKAN_SDK=%~dp0\VulkanSDK
 )
 
-cd /d "%~dp0" 
+
+cd /d "%~dp0"
 
 if not exist "%CD%\build" (
-	echo building the "build" directory...
+  echo building the "build" directory...
 	
-	mkdir build
+  mkdir build
 )
 
 cd build
