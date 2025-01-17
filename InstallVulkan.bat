509ed6548e939a82c07ebee8a7e7def830376960
Kaylubkiss <Kaylubkiss@users.noreply.github.com>
2025-01-16 16:35:56 -0800

    fixed up the batch script

---
+++ InstallVulkan.bat
@@ -0,0 +1,28 @@
+@echo off
+
+
+if defined VULKAN_SDK (
+	echo Vulkan Dependency Found...
+) else (
+	echo VulkanSDK not found, downloading to your repository...
+
+	curl https://sdk.lunarg.com/sdk/download/1.4.304.0/windows/VulkanSDK-1.4.304.0-Installer.exe --output VulkanInstaller.exe
+
+	VulkanInstaller.exe --accept-licenses --default-answer --root "%~dp0\VulkanSDK" --confirm-command install com.lunarg.vulkan.32bit com.lunarg.vulkan.sdl2 com.lunarg.vulkan.glm com.lunarg.vulkan.vma com.lunarg.vulkan.debug com.lunarg.vulkan.debug32
+)
+
+cd /d "%~dp0" 
+
+if not exist "%CD%\build" (
+	echo building the "build" directory...
+	
+	mkdir build
+)
+
+cd build
+
+cmake ..
+
+cd ..
+
+PAUSE
