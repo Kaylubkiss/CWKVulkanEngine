9818e8f3ff5f79be9ff4189f32d9e1f0f14d0f98
Kaylubkiss <kaylubkiss@gmail.com>
2026-01-27 13:52:45 -0600

    added glm as a submodule in the project. TODO: toggle a bunch of useless CMAKE options from SDL and other libraries.

--- windows_build.bat
+++ windows_build.bat
@@ -4,22 +4,19 @@
 winget install --id=KhronosGroup.VulkanSDK  -e
 
 ::make sure cmake is installed.
-winget install --id Kitware.CMake -e
+winget install --id=Kitware.CMake -e
 
-::make sure fastgltf is installed
-vcpkg install fastgltf
+::make sure to have ninja for build generation
+winget install --id=Ninja-build.Ninja -e
 
 set previous_directory=%cd%
 
 cd %VULKAN_SDK%
 
 maintenancetool.exe --accept-licenses --default-answer --confirm-command install ^
-com.lunarg.vulkan.sdl2 ^
-com.lunarg.vulkan.glm ^
 com.lunarg.vulkan.volk ^
 com.lunarg.vulkan.vma ^
-com.lunarg.vulkan.debug ^
-com.lunarg.vulkan.arm64
+com.lunarg.vulkan.debug
 
 cd %previous_directory%
 
@@ -28,5 +25,7 @@ mkdir build
 cd build
 
 cmake ..
+
+ninja
 
 pause
